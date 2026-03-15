3f75e9995109ad66b972d5164e3c962a96c3286f
naxocist <subforwork@gmail.com>
2026-03-16 02:03:39 +0700

    update

--- lua/custom/plugins/looks.lua
+++ lua/custom/plugins/looks.lua
@@ -1,15 +1,43 @@
 return {
   {
+    "y3owk1n/base16-pro-max.nvim",
+    config = function()
+      require("base16-pro-max").setup({
+        colors = {
+          base00 = "#1f1f28",
+          base01 = "#2a2a37",
+          base02 = "#3a3a4e",
+          base03 = "#4e4e5e",
+          base04 = "#9e9eaf",
+          base05 = "#c5c5da",
+          base06 = "#dfdfef",
+          base07 = "#e6e6f0",
+          base08 = "#ff5f87",
+          base09 = "#ff8700",
+          base0A = "#ffaf00",
+          base0B = "#5fff87",
+          base0C = "#5fd7ff",
+          base0D = "#5fafff",
+          base0E = "#af87ff",
+          base0F = "#d7875f",
+        },
+        styles = { italic = false, transparency = true },
+        plugins = { enable_all = true },
+      })
+      vim.cmd.colorscheme("base16-pro-max")
+    end,
+  },
+  {
     "vague-theme/vague.nvim",
     lazy = false,
     priority = 500,
     config = function()
       require("vague").setup({
-        bold = true,
+        bold = false,
         italic = false,
         transparent = true,
       })
-      vim.cmd("color vague")
+      -- vim.cmd("color vague")
     end,
   },
   {
